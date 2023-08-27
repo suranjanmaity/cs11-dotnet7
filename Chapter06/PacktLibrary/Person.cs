182437c512270cbe4cd10f6fd666ce93e889e30f
suranjanmaity <suranjanmaity4@gmail.com>
2023-08-27 18:16:13 +0530

    Setting up a class library and console application

---
+++ Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Packt.Shared;
+
+public class Person : object
+{
+    // properties
+    public string? Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+    // methods
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+}
---
+++ Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using Packt.Shared;
+
+Person harry = new()
+{
+    Name = "Harry",
+    DateOfBirth = new(year: 2001, month: 3, day: 25)
+};
+harry.WriteToConsole();
