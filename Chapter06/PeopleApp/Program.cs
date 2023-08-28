821a3f4ee28d9cce4b4e27a868acba6c968a5e91
suranjanmaity <suranjanmaity4@gmail.com>
2023-08-28 11:47:16 +0530

    Working with non generic types

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -6,3 +6,15 @@ Person harry = new()
     DateOfBirth = new(year: 2001, month: 3, day: 25)
 };
 harry.WriteToConsole();
+
+//non-generic lookup collection
+System.Collections.Hashtable lookupObject = new();
+lookupObject.Add(key: 1, value: "Alpha");
+lookupObject.Add(key: 2, value: "Beta");
+lookupObject.Add(key: 3, value: "Gamma");
+lookupObject.Add(key: harry, value: "Delta");
+int key = 2; // look up the value that has 2 as its key
+WriteLine(format: "{0} has value: {1}",
+    arg0: key, arg1: lookupObject[key]);
+WriteLine(format: "{0} has value: {1}",
+    arg0: harry, arg1: lookupObject[harry]);
