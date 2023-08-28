8bc97e1845942cb165fc2ab5252faaddbdcdd0c0
suranjanmaity <suranjanmaity4@gmail.com>
2023-08-28 12:00:21 +0530

    working with generic types

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -18,3 +18,12 @@ WriteLine(format: "{0} has value: {1}",
     arg0: key, arg1: lookupObject[key]);
 WriteLine(format: "{0} has value: {1}",
     arg0: harry, arg1: lookupObject[harry]);
+//generic lookup collection
+Dictionary<int, string> lookupIntString = new();
+lookupIntString.Add(key: 1, value: "Alpha");
+lookupIntString.Add(key: 2, value: "Beta");
+lookupIntString.Add(key: 3, value: "Gamma");
+lookupIntString.Add(key: 4, value: "Delta");
+key = 3;
+WriteLine(format:"key {0} has value: {1}",
+    arg0:key, arg1: lookupIntString[key]);
