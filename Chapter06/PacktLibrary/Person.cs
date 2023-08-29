5b5cdd96c0ec98bf6a410e89ca8508dd68d7c328
suranjanmaity <suranjanmaity4@gmail.com>
2023-08-29 17:31:22 +0530

    Comparing objects when sorting

--- Chapter06/PacktLibrary/Person.cs
+++ Chapter06/PacktLibrary/Person.cs
@@ -1,6 +1,6 @@
 ﻿namespace Packt.Shared;
 
-public class Person : object
+public class Person : IComparable<Person?>
 {
     // properties
     public string? Name { get; set; }
@@ -33,5 +33,44 @@ public class Person : object
                 Shout(this, EventArgs.Empty);
             }
         }
+    }
+
+    public int CompareTo(Person? other)
+    {
+        int position;
+        if ((this is not null) && (other is not null))
+        {
+            if ((Name is not null) && (other.Name is not null))
+            {
+                //if both Name values are not null,
+                // use the string implementation of CompareTo
+                position = Name.CompareTo(other.Name);
+            }
+            else if ((Name is not null) && (other.Name is null))
+            {
+                position = -1; // else this Person precedes other Person
+            }
+            else if ((Name is null) && (other.Name is not null))
+            {
+                position = 1; // else this Person follows other Person
+            }
+            else
+            {
+                position = 0; // this Person and other Person are at same position
+            }
+        }
+        else if ((this is not null) && (other is null))
+        {
+            position = -1; // this Person precedes other Person
+        }
+        else if ((this is null) && (other is not null))
+        {
+            position = 1; // this Person follows other Person
+        }
+        else
+        {
+            position = 0; // this Person and other Person are at same position
+        }
+        return position;
     }
 }
--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -37,3 +37,17 @@ harry.Poke();
 harry.Poke();
 harry.Poke();
 harry.Poke();
+
+// Inplementing interface
+Person?[] people =
+{
+    null,
+    new(){Name= "Simon" },
+    new(){Name = "Jenny" },
+    new(){Name = "Adam" },
+    new(){Name = null },
+    new(){Name = "Richard" }
+};
+OutputPeopleNames(people, "Initial list of people:");
+Array.Sort(people);
+OutputPeopleNames(people, "After sorting using Person's IComparable implementation:");
